f50a9d7a3c48b85fa3dcfa889b71bf86725367ca
Anshul <anshul.anshul.rawat@gmail.com>
2018-10-10 23:28:37 +0530

    preparing for season finale

---
+++ app/src/main/java/com/kinitoapps/punjabgovttest/CompanyProfileActivity.java
@@ -0,0 +1,15 @@
+package com.kinitoapps.punjabgovttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompanyProfileActivity extends AppCompatActivity {
+    String URL_SPINNER = "https://governmentappcom.000webhostapp.com/spinner.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_profile);
+
+    }
+}
